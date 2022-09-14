56ae8bbf4d27960bebbddfbcc68687fed98bc0ee
vishnu <vishnuvardhan.2005@gmail.com>
2022-09-13 23:10:03 -0700

    register validation and hash password wit bcrypt.hashpw

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import bcrypt
 
 app = Flask(__name__)
 
@@ -27,8 +28,16 @@ def register_user():
         email = request.form.get('email')
         password = request.form.get('password')
 
+        #check if email already exists
+        result = users.find_one({"email": email})
+        if result:
+            return "User exists"
+
+        #hash the password
+        hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
         #validate input
-        user_input = {'name': name, 'email': email, 'password': password}
+        user_input = {'name': name, 'email': email, 'password': hash_password}
 
         users.insert_one(user_input)
         return "record inserted"
