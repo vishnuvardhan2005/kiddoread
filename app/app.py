fc1a7ed36a10edd2ed135fdd49dd754364ae9f4c
vishnu <vishnuvardhan.2005@gmail.com>
2022-09-15 07:35:24 -0700

    login api

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
-import bcrypt
+from flask_bcrypt import Bcrypt
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -11,6 +11,7 @@ app = Flask(__name__)
 # Setup the Flask-JWT-Extended extension
 app.config["JWT_SECRET_KEY"] = "this-is-a-mission"
 jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 
 try:
     client = MongoClient('mongodb://localhost:27017/')
@@ -42,7 +43,8 @@ def register_user():
             return "User exists"
 
         #hash the password
-        hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        #hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        hash_password = bcrypt.generate_password_hash(password)
 
         #validate input
         user_input = {'name': name, 'email': email, 'password': hash_password}
@@ -60,9 +62,18 @@ def login_user():
         password = request.form.get('password')
 
         #validations
-        if password != "password":
-            return "Invalid user"
-        
+        #result = users.find_one({'email':email, 'password':hash_password})
+
+        result = users.find_one({'email':email})
+
+        if not result:
+            return "Invalid credentials"
+
+        hash_password = result['password']
+
+        if bcrypt.check_password_hash(hash_password, password) == False: #
+            return "Invalid password"
+
         access_token = create_access_token(identity=email)
         return jsonify(access_token=access_token)
 
@@ -70,9 +81,9 @@ def login_user():
 @jwt_required()
 def question():
     if request.method == 'GET':
-        return "Get question"
+        return "Get question, restricted"
     else:
-        return ""
+        return "Update question, restricted"
 
 
 if __name__ == '__main__':
